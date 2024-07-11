7df3f5b1522dff5111d568b004535f78e04b75aa
deldmi <andre88444@gmail.com>
2024-07-11 18:54:25 -0300

    config do db

--- backend/src/routes/create-trip.ts
+++ backend/src/routes/create-trip.ts
@@ -1,10 +1,31 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import z from "zod";
+import { prisma } from "../lib/prisma";
+
 
 
 export async function createTrip(app: FastifyInstance) {
-    app.withTypeProvider<ZodTypeProvider>().post('/trips', async () => {
-        return 'Hello Word!';
-    })
+    app.withTypeProvider<ZodTypeProvider>().post('/trips', {
+        schema: {
+            body: z.object({
+                destination: z.string().min(4),
+                starts_at: z.coerce.date(),
+                ends_at: z.coerce.date(),
+            })
+        }
+    }, async (req, rep) => {
+        const { destination, ends_at, starts_at } = req.body;
+
+        const trip = await prisma.trip.create({
+            data: {
+                destination,
+                starts_at,
+                ends_at,
+            }
+        })
+
+        
+        return { tripId: trip.id };
+    });
 }
