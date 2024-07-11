4fc8e4d6a1ce628fef3177f09b548a0347303542
deldmi <andre88444@gmail.com>
2024-07-11 19:02:06 -0300

    validação de data

--- backend/src/routes/create-trip.ts
+++ backend/src/routes/create-trip.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
-import z from "zod";
+import dayjs from "dayjs";
+import { z } from "zod";
 import { prisma } from "../lib/prisma";
 
 
@@ -16,6 +17,14 @@ export async function createTrip(app: FastifyInstance) {
         }
     }, async (req, rep) => {
         const { destination, ends_at, starts_at } = req.body;
+
+        if (dayjs(starts_at).isBefore(new Date())) {
+            throw new Error('Invalid trip start date.')
+        }
+
+        if ( dayjs(ends_at).isBefore(starts_at)) {
+            throw new Error('Invalid trip and date.')
+        }
 
         const trip = await prisma.trip.create({
             data: {
